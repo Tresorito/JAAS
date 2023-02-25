077cf0931d4c136c09f5f99156089d3f5a2f9dd8
tresorito <kaletoadoss@gmail.com>
2023-02-26 00:58:42 +0100

    jaas + EmailSender

---
+++ main/java/ma/tresor/jaasPlusEmail/JaasPlusEmailApplication.java
@@ -0,0 +1,13 @@
+package ma.tresor.jaasPlusEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaasPlusEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaasPlusEmailApplication.class, args);
+	}
+
+}
