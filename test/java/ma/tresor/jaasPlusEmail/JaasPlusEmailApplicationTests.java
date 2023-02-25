72d93e97a3f8c9a61c2cc763121c4a89ea6d7241
tresorito <kaletoadoss@gmail.com>
2023-02-26 00:49:57 +0100

    jaas + EmailSender

---
+++ test/java/ma/tresor/jaasPlusEmail/JaasPlusEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.tresor.jaasPlusEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaasPlusEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
